12e64efbab0f8e82666757f2715cb103fdaf8a69
Tomasz Mroczkowski <tomasz.mroczkowski@lingarogroup.com>
2025-07-02 20:35:12 +0200

    fixes: - black fixes

--- src/config.py
+++ src/config.py
@@ -1,8 +1,10 @@
 import os
 
-env = os.getenv('APP_ENV', 'dev')
+env = os.getenv("APP_ENV", "dev")
 
-if (env == 'test'):
+if env == "test":
     SQLALCHEMY_DATABASE_URL = "sqlite:///./test/test.db"
 else:
-    SQLALCHEMY_DATABASE_URL = "postgresql://postgres:lingaroPassSql@db:5432/task_tracker_db"
+    SQLALCHEMY_DATABASE_URL = (
+        "postgresql://postgres:lingaroPassSql@db:5432/task_tracker_db"
+    )
